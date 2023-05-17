d01a8fe839910e9e8304d8b97c4b3834770007ff
Jason McGrath <jnomad21@yahoo.com>
2023-05-17 12:32:00 -0700

    heroku static

--- funko_collector/settings.py
+++ funko_collector/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,7 +132,7 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 #STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
 
